75d65c891a34d60782b2cfb90b65c3b0882b06f2
yanchenjian <yanchenjian@youzan.com>
2020-12-16 14:46:47 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import sum from "./sum.js"
+import {minus} from "./minus.js";
+
+const value = sum(1,2);
+const division = minus(2,1);
+
+console.log(value);
+console.log(division);
---
+++ src/sum.js
@@ -0,0 +1,5 @@
+import add from "./add.js";
+
+export default (a, b) => {
+  return add(a, b);
+};
